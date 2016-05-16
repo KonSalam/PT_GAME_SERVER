bbd393a1af5c5ccb49da9f20cda3ddf8e7b9558f
Konrad Salamon <konradsalamon95@gmail.com>
2016-05-16 11:30:47 +0200

    Server as Class

---
+++ Serwer/Server.h
@@ -0,0 +1,44 @@
+#ifndef Server_H
+#define Server_H
+
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <string>
+#include <thread>
+#include <vector>
+
+#pragma comment (lib, "Ws2_32.lib")
+#define IP_ADDRESS "127.0.0.1"
+#define DEFAULT_PORT "2556"
+#define DEFAULT_BUFLEN 1024
+
+struct client_type
+{
+	int id;
+	SOCKET socket;
+};
+
+const char OPTION_VALUE = 1;
+const int MAX_CLIENTS = 4;
+
+class Server
+{
+private:
+	WSADATA wsaData;
+	struct addrinfo hints;
+	struct addrinfo *server = NULL;
+	SOCKET server_socket = INVALID_SOCKET;
+	std::vector<client_type> client;
+	std::thread my_thread[MAX_CLIENTS];
+
+	int num_clients = 0;
+	int temp_id = -1;
+
+public:
+	void init();//inicjazliacja servera
+	void run();
+};
+
+
+#endif
